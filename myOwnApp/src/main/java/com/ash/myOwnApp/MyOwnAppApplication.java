376edb4701091f5a360ab6da14c3cca94bf8d83f
Aesha Patel <Aesha Patel@ash>
2024-06-25 13:58:43 +0530

    First Version

---
+++ myOwnApp/src/main/java/com/ash/myOwnApp/MyOwnAppApplication.java
@@ -0,0 +1,13 @@
+package com.ash.myOwnApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOwnAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyOwnAppApplication.class, args);
+	}
+
+}
---
+++ myOwnApp/src/test/java/com/ash/myOwnApp/MyOwnAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ash.myOwnApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOwnAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
